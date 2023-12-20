cbe7174335077803f4a2ca45d5e5ec39cac5f6b0
Brandon Phillips <phizzl3@gmail.com>
2023-12-20 08:23:20 -0600

    Rename -Module -> dropped_file_getter.py -Function -> get_dropped_file

--- dropped_file_getter.py
+++ dropped_file_getter.py
@@ -1,21 +1,32 @@
-# 10.29.2021
+"""
+
+Gets file/directory via drag and drop into a console window and 
+return a Path object.
+
+Asks for file or directory via drag and drop, strips unneeded characters,
+and uses pathlib.Path to return a Path object.
+
+Rev: 12/20/2023
+
+"""
 
 from pathlib import Path
 
 
-def get():
+def get_dropped_file():
     """
-    Gets file/directory via drag and drop and return a Path object.
+    Gets file/directory via drag and drop into a console window and 
+    return a Path object.
 
     Asks for file or directory via drag and drop, strips unneeded characters,
     and uses pathlib.Path to return a Path object.
 
     Returns:
     - (pathlib.PosixPath object): A pathlib.PosixPath object that can be 
-    used to get file path, filname, extension, etc. 
+    used to get file path, file name, extension, etc. 
 
     Sample Usage:
-    - p = dropfile.get()
+    - p = get_dropped_file()
         - p = full path
         - p.parent = parent folder
         - p.stem = filename excluding extension
