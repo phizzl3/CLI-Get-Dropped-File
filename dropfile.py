47ef27c7a2a7da712cd35cb78c8ab72b075d8569
Brandon Phillips <phizzl3@gmail.com>
2024-03-02 22:04:29 -0600

    Add/Rename functions

--- dropfile.py
+++ dropfile.py
@@ -1,46 +1,16 @@
-"""
-
-Gets file/directory via drag and drop into a console window and 
-return a Path object.
-
-Asks for file or directory via drag and drop, strips unneeded characters,
-and uses pathlib.Path to return a Path object.
-
-"""
-
-__version__ = "1.0.0"
-
+__version__ = "1.1.0"
 
 from pathlib import Path
 
 
-def getfile():
-    """Gets file/directory via drag and drop into a console window and 
-    return a Path object.
+def _clean_characters(file_location: str) -> str:
+    file_location = file_location.strip(" &'\"")
+    file_location = file_location.replace("\ ", " ")
+    file_location = file_location.replace("''", "'")
+    return file_location
 
-    Asks for file or directory via drag and drop, strips unneeded characters,
-    and uses pathlib.Path to return a Path object.
 
-    Returns:
-    - (pathlib.PosixPath object): A pathlib.PosixPath object that can be 
-    used to get file path, file name, extension, etc. 
-
-    Sample Usage:
-    - p = getfile()
-        - p = full path
-        - p.parent = parent folder
-        - p.stem = filename excluding extension
-        - p.suffix = file extension
-    """
-    # Get input file/folder and strip characters
-    file_drop = input("\n Drop File: ")
-
-    # Clean left and right characters
-    file_drop = file_drop.strip(" &'\"")
-
-    # Clean Windows character issues inside string
-    file_drop = file_drop.replace("\ ", " ")
-    file_drop = file_drop.replace("''", "'")
-
-    # Set Path object and return
-    return Path(file_drop).resolve()
+def get_dropped_file() -> Path:
+    file_location = input("\n Drop File: ")
+    file_location = _clean_characters(file_location)
+    return Path(file_location).resolve()
