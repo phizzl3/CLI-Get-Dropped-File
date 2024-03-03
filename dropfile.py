ed2de5b038f5bbae8222cc23cb64638ec43b3db7
Brandon Phillips <phizzl3@gmail.com>
2024-03-03 17:10:56 -0600

    Update private function

--- dropfile.py
+++ dropfile.py
@@ -1,16 +1,16 @@
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from pathlib import Path
 
 
-def _clean_characters(file_location: str) -> str:
+def get_dropped_file() -> Path:
+    file_location = input("\n Drop File: ")
+    file_location = __clean_characters(file_location)
+    return Path(file_location).resolve()
+
+
+def __clean_characters(file_location: str) -> str:
     file_location = file_location.strip(" &'\"")
     file_location = file_location.replace("\ ", " ")
     file_location = file_location.replace("''", "'")
     return file_location
-
-
-def get_dropped_file() -> Path:
-    file_location = input("\n Drop File: ")
-    file_location = _clean_characters(file_location)
-    return Path(file_location).resolve()
